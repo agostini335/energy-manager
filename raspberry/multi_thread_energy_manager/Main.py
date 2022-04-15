d0925b49e2ba13c61fd60e60ab91cd50b8d1b47e
Andrea Agostini <agostini95@gmail.com>
2022-04-15 17:07:47 +0200

    Update Main.py

--- raspberry/multi_thread_energy_manager/Main.py
+++ raspberry/multi_thread_energy_manager/Main.py
@@ -91,7 +91,8 @@ def mod_manager(reading_lock,mod_lock):
         current_reading = last_reading.get_copy()
         reading_lock.release()
         state_manager.handle_reading(current_reading)
-        logging.info("STATE: "+str(state_manager._state.name)+ "now:"+str(datetime.now())+" current reading: "+str(current_reading.last_update) +" fresh: "+str(current_reading.is_fresh))
+        if current_reading.is_fresh:
+            logging.info("STATE: "+str(state_manager._state.name)+ "now:"+str(datetime.now())+" current reading: "+str(current_reading.last_update) +" fresh: "+str(current_reading.is_fresh))
                 
 
 
