f259d25274193e7cf533ba92bae3a1e080d60f77
Andrea Agostini <agostini95@gmail.com>
2022-04-22 00:21:49 +0200

    Update buttonExample.py

--- raspberry/optimized_energy_manager copy/buttonExample.py
+++ raspberry/optimized_energy_manager copy/buttonExample.py
@@ -1,8 +1,8 @@
 from DisplayManager import DisplayManager
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 display_manager = DisplayManager()
 import time
 from datetime import datetime
