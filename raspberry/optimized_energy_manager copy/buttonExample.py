fb3e43631076772202608489b610d7f303790697
Andrea Agostini <agostini95@gmail.com>
2022-04-21 23:44:10 +0200

    Update buttonExample.py

--- raspberry/optimized_energy_manager copy/buttonExample.py
+++ raspberry/optimized_energy_manager copy/buttonExample.py
@@ -5,12 +5,24 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 display_manager = DisplayManager()
 import time
-i = 0
+from datetime import datetime
+from itertools import cycle
+
+lst = ['AUTO', 'ON', 'OFF']
+
+pool = cycle(lst)
+
+k=2
+list
 while True:
     if GPIO.input(10) == GPIO.HIGH:
-        print('pressed '+str(i))
+        i = next(pool)
+        print('pressed '+i)
         display_manager.driver_print(str(i),1)
+        pressed = datetime.now()
         time.sleep(1)
-        i +=1
+    if pressed-datetime.now()>k:
+        display_manager.driver_print("pressed",2)
+
 
 
