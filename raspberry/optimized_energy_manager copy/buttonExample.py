6c7a8bbf0d038916ab382a5cf7e0b8aa8e99eb7d
Andrea Agostini <agostini95@gmail.com>
2022-04-22 17:31:02 +0200

    Update buttonExample.py

--- raspberry/optimized_energy_manager copy/buttonExample.py
+++ raspberry/optimized_energy_manager copy/buttonExample.py
@@ -1,6 +1,6 @@
-'''
 from DisplayManager import DisplayManager
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.cleanup()
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
@@ -39,3 +39,4 @@ GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 GPIO.add_event_detect(15,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
+'''
