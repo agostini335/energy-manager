a62c4ad70597bb96f9917a33869b94271a042f72
Andrea Agostini <agostini95@gmail.com>
2022-04-22 17:18:18 +0200

    Update buttonExample.py

--- raspberry/optimized_energy_manager copy/buttonExample.py
+++ raspberry/optimized_energy_manager copy/buttonExample.py
@@ -1,3 +1,4 @@
+'''
 from DisplayManager import DisplayManager
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
@@ -28,6 +29,13 @@ while True:
         display_manager.driver_print(i,2)
         display_manager.driver_print(str(datetime.now()),3)
         premuto=False
-
-
-
+'''
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+def button_callback(channel):
+    print("Button was pushed!")
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
